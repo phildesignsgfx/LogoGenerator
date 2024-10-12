704278615f49896f271882eece52c0dbade27744
Philippe Moise <82619578+phildesignsgfx@users.noreply.github.com>
2024-10-12 23:53:10 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
 import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg"; // Including image (though not used in this example)
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <form>
+        <p>Welcome</p>
+        <input type="email" placeholder="Email" /><br />
+        <input type="password" placeholder="Password" /><br />
+        <input type="button" value="Sign in" /><br />
+        <a href="#">Forgot Password?</a>
+      </form>
+
+      <div className="drops">
+        <div className="drop drop-1"></div>
+        <div className="drop drop-2"></div>
+        <div className="drop drop-3"></div>
+        <div className="drop drop-4"></div>
+        <div className="drop drop-5"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,84 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 const port = 3000;
+
 let publicUrl = `ws://localhost:${port}/ws`;
-//only for gitpod
-if(process.env.GITPOD_WORKSPACE_URL){
+
+// Adjust the public URL for Gitpod
+if (process.env.GITPOD_WORKSPACE_URL) {
   const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
   publicUrl = `wss://${port}-${host}/ws`;
 }
-//only for codespaces
-if(process.env.CODESPACE_NAME){
+
+// Adjust the public URL for Codespaces
+if (process.env.CODESPACE_NAME) {
   publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.app.github.dev/ws`;
 }
 
 module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: './src/js/index.js', // Entry point
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/, // Babel transpiler for JS/JSX files
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/, // CSS loader
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/, // SCSS loader
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader',   // Translates CSS into CommonJS modules
+          'sass-loader',  // Compiles SCSS to CSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/, // Image loader
+        use: {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' },
         },
-        {
-          test: /\.(css)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|svg|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)$/, // Font loader
+        use: ['file-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx', '.scss'], // Added SCSS to extensions
   },
-  devtool: "source-map",
+  devtool: 'source-map', // Source maps for easier debugging
   devServer: {
     port,
     hot: true,
-    allowedHosts: "all",
+    allowedHosts: 'all',
     historyApiFallback: true,
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, 'dist'), // Ensure static assets are served
     },
     client: {
-      webSocketURL: publicUrl
+      webSocketURL: publicUrl, // Adjust WebSocket URL based on the environment
     },
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    // new ESLintPlugin({
-    //   files: path.resolve(__dirname, "src"),
-    // }),
+    new webpack.HotModuleReplacementPlugin(), // Hot module replacement for live reload
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: '4geeks.ico',
+      template: 'template.html', // Template HTML file
     }),
-  ]
+    new ErrorOverlayPlugin(), // Better error overlay for development
+  ],
 };
